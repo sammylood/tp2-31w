7158ad48ba544964aa4adc6a1a8393fd3b23899c
Sammylood <sammy.romeo@hotmail.com>
2024-10-17 22:55:40 -0400

    s2c2 Integration de la navigation principale

---
+++ footer.php
@@ -0,0 +1,11 @@
+    <footer class="pied">
+        <section class="global">
+            <div>1</div>
+            <div>Auteur : Samuel Dorneval</div>
+            <div>3</div>
+        </section>
+    </footer>
+    <?php wp_footer() ?>
+    </body>
+
+    </html>
--- index.php
+++ index.php
@@ -6,38 +6,29 @@
 ?>
 <?php get_header(); ?>
 
-    <main class="principal">
-        <section class="global"></section>
+<main class="principal">
+    <section class="global">
         <h2>Liste de cours</h2>
-       <div class="principal__conteneur">
-        <?php if (have_posts()):   /*permet d'extraire les articles*/ ?>
-            <?php while (have_posts()): the_post(); /* extrait un post */ ?>
-                <?php
+        <div class="principal__conteneur">
+            <?php if (have_posts()):   /*permet d'extraire les articles*/ ?>
+                <?php while (have_posts()): the_post(); /* extrait un post */ ?>
+                    <?php
                     $chaine = get_the_title();
                     $sigle = substr($chaine, 0, 7);
-                    $titre = substr($chaine, 8, stripos($chaine,"(") - 8);
-                ?>
-                <article class="principal__article">
-                    <h3> 
-                        <?php echo $sigle; /* fonctionne seulement si the_post() est exécuté  / get_ sont des fonctions qui retournent des chaines de caracteres*/ ?>
-                    </h3>
-                    <h4> 
-                        <?php echo $titre; /* fonctionne seulement si the_post() est exécuté  / get_ sont des fonctions qui retournent des chaines de caracteres*/ ?>
-                    </h4>
-                    <p><?php echo wp_trim_words(get_the_excerpt(), 20, null);   /* fonctionne seulement si the_post() est exécuté */ ?></p>
-                </article>
-            <?php endwhile; ?>
+                    $titre = substr($chaine, 8, stripos($chaine, "(") - 8);
+                    ?>
+                    <article class="principal__article">
+                        <h3>
+                            <?php echo $sigle; /* fonctionne seulement si the_post() est exécuté  / get_ sont des fonctions qui retournent des chaines de caracteres*/ ?>
+                        </h3>
+                        <h4 class="principal__article--title">
+                            <?php echo $titre; /* fonctionne seulement si the_post() est exécuté  / get_ sont des fonctions qui retournent des chaines de caracteres*/ ?>
+                        </h4>
+                        <p><?php echo wp_trim_words(get_the_excerpt(), 20, null);   /* fonctionne seulement si the_post() est exécuté */ ?></p>
+                    </article>
+                <?php endwhile; ?>
         </div>
-        <?php endif ?>
-    </main>
-    <footer class="pied">
-        <section class="global">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-        </section>
-    </footer>
-    <?php wp_footer() ?>
-</body>
-
-</html>
+    <?php endif ?>
+    </section>
+</main>
+<?php get_footer() ?>
