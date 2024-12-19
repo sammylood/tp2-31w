a0d85b4f77ea210370744c51b8c0c3fe0993326a
Sammylood <sammy.romeo@hotmail.com>
2024-12-19 13:57:16 -0500

    Ajout liens dans le footer

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
     <footer class="pied">
         <section class="global">
             <div></div>
-            <div>© Voyages Just Travel <?php echo date('Y'); ?> </div>
+            <div>© Voyages Just Travel <?php echo date('Y'); ?> <br><a href="https://github.com/sammylood/31w/tree/EF">Branche EF de mon theme</a>
+                <a href="https://github.com/sammylood/">Lien vers filtres pays</a>
+            </div>
             <div></div>
         </section>
     </footer>
